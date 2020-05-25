7c1d8b22903cd0894fad6e4bf93e1e4f1fd8cf3e
pranavv481 <pranavv481@gmail.com>
2020-05-25 14:21:44 +0530

    increment, deecrement, reset

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import HookCounterTwo from './components/HookCounterTwo';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <HookCounterTwo />
+      </div>
+    );
+  }
+}
+
+export default App;
